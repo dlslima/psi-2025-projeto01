a8c6e2ae1b9a9f7300e80d23f30d98231d4dba94
Danilo de Lima da Silva <20231181110017@ifrn.local>
2026-01-29 12:47:13 -0300

    parte 2 final

--- app/admin.py
+++ app/admin.py
@@ -5,21 +5,26 @@ from .models import Ator, Inicio, Sobre
 
 @admin.register(Inicio)
 class InicioAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "subtitulo", "ativo", "atualizado_em")
-    list_filter = ("ativo",)
+    list_display = ("titulo", "subtitulo", "ativo", "criado_em", "atualizado_em")
+    list_filter = ("ativo", "criado_em")
     search_fields = ("titulo", "subtitulo", "info")
+    readonly_fields = ("criado_em", "atualizado_em")
 
 
 @admin.register(Ator)
 class AtorAdmin(admin.ModelAdmin):
-    list_display = ("ordem", "nome", "numero", "idade", "ativo")
-    list_filter = ("ativo",)
+    list_display = ("ordem", "nome", "numero", "idade", "ativo", "criado_em")
+    list_display_links = ("nome",)
+    list_filter = ("ativo", "criado_em")
     search_fields = ("nome", "numero", "nascimento", "resumo")
     ordering = ("ordem", "nome")
+    readonly_fields = ("criado_em", "atualizado_em")
+    list_editable = ("ordem", "ativo")
 
 
 @admin.register(Sobre)
 class SobreAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "subtitulo", "ativo", "atualizado_em")
-    list_filter = ("ativo",)
+    list_display = ("titulo", "subtitulo", "ativo", "criado_em", "atualizado_em")
+    list_filter = ("ativo", "criado_em")
     search_fields = ("titulo", "subtitulo", "info", "criadores")
+    readonly_fields = ("criado_em", "atualizado_em")
